74632f651bcf448c934d66317734922d339028d9
Daniel Costa <109314570+danielcostadev@users.noreply.github.com>
2023-06-18 21:02:27 -0300

    Update aula02.java
    Correção na criação da instância do objeto c02

--- aula02.java
+++ aula02.java
@@ -14,17 +14,15 @@ public class Aula02 {
         c01.ponta = 0.5f;
         c01.tampada = false;
         c01.status();
-        
+                
         Caneta c02 = new Caneta();
-        
-        c01.modelo = "Compactor";
-        c01.cor = "Azul";
-        c01.tipo= "Esferográfica";
-        c01.carga = 40;
-        c01.ponta = 0.7f;
-        c01.tampada = true;
-        c01.status();
-        
+        c02.modelo = "Crown Master Chrome";
+        c02.cor = "Azul";
+        c02.tipo= "Esferográfica";
+        c02.carga = 100;
+        c02.ponta = 0.7f;
+        c02.tampada = true;
+        c02.status();
     }
     
 }
